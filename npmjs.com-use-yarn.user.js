dce59e2c401b8f18b6a6e7962d10ea36f4571e25
Peanutbother <6437182+peanutbother@users.noreply.github.com>
2022-04-04 09:26:11 +0200

    add icon for userscript

--- npmjs.com-use-yarn.user.js
+++ npmjs.com-use-yarn.user.js
@@ -6,6 +6,7 @@
 // @author       Bricksoft
 // @downloadURL  https://raw.githubusercontent.com/bricksoft/npmjs.com-use-yarn/master/npmjs.com-use-yarn.user.js
 // @updateURL    https://raw.githubusercontent.com/bricksoft/npmjs.com-use-yarn/master/npmjs.com-use-yarn.user.js
+// @icon         https://static.npmjs.com/da3ab40fb0861d15c83854c29f5f2962.png
 // @match        *://www.npmjs.com/package/*
 // @grant        none
 // ==/UserScript==
